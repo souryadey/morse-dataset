a46a5f6718a4f6f72aed6feacbe9b67fe246d098
souryadey <souryade@usc.edu>
2018-04-30 15:35:36 -0700

    updates and fixes

--- README.md
+++ README.md
@@ -1,38 +1,47 @@
 # morse-dataset
-Generate Morse code datasets for training artificial neural networks
+Generate Morse code datasets for machine learning algotihms such as artificial neural networks
 
-For a complete description and motivation, refer to my blog: 
+Complete description and motivation soon to follow after I finish writing a paper on this. My blog has a fairly nice description : 
 
 Here is a more succinct description of the Python script:
 
-	Style='BW': Black and white having 0s for spaces and 1s for dot or dash. Noise means bit flips
-	Style='GRAY': Gaussian grayscale levels. Noise is additive Gaussian
+	Create morse code datasets:
+	        Style='BW': Black and white having 0s for spaces and 1s for dot or dash. Noise means bit flips
+	        Style='GRAY': Gaussian grayscale levels. Noise is additive Gaussian
+	        save_filename: Leave as '' to save in preset path inside
 
-	Framelen: Total length of frame for 1 character, i.e. 1 input
-	Classes: How many characters, i.e. outputs
-	TReach,VAeach,TEeach: No. of training, validation and test cases FOR EACH class
-	minlendot,maxlendot,... : min and max length of dots, dashes and intermediate spaces
-	leadingsp_rand: Set to 0 to have no leading spaces, otherwise 1 to have random number of leading spaces
+	    Framelen: Total length of frame for 1 character
+	    Classes: How many characters
+	    TReach,VAeach,TEeach: No. of training, validation and test cases FOR EACH class
+	    minlendot,maxlendot,... : min and max length of dots, dashes and intermediate spaces
+	    leadingsp_rand: Set to 0 to have no leading spaces, otherwise 1 to have random number of leading spaces
+	    dilation: If >1, all lengths are increased by this factor
 
-	Black and white only:
-    	maxflip: Noise measure. Max how many bits to flip
-    	SET maxfilp=0 for NO NOISE
-
-	Grayscale only:
-    	levels: How many levels for dots and dashes. Will be normalized at end
-    	symbmean: Mean level for any symbol (dot or dash)
-    	symbsd: Standard deviation for symbol levels
-    	noisemean: Mean level for noise
-    	noisesd: Standard deviation for noise
-    	SET noisesd=0 for NO NOISE
+	    Black and white only:
+	        maxflip: Noise measure. Max how many bits to flip
+	        SET maxflip=0 for NO NOISE
+	    Grayscale only:
+	        levels: How many levels for dots and dashes. Will be normalized at end
+	        symbmean: Mean level for any symbol (dot or dash)
+	        symbsd: Standard deviation for symbol levels
+	        noisemean: Mean level for noise
+	        noisesd: Standard deviation for noise
+	        SET noisesd=0 for NO NOISE
 	
 
-An already generated dataset using the default grayscale parameters is included as morse_dataset.npz. Use these commands to extract the data and labels into training, validation and test:
+2 already generated datasets are included:
+	baseline.npz : Uses default parameters
+	difficult.npz : Uses noisesd=4, leadingsp_rand=1, minlendash=3
+Use these commands to extract the data and labels into training, validation and test:
 
-	data = np.load('./morse_dataset.npz')
+	data = np.load('./baseline.npz')
 	xtrain = data['xtr']
 	ytrain = data['ytr']
 	xval = data['xva']
 	yval = data['yva']
 	xtest = data['xte']
 	ytest = data['yte']
+	
+
+For a guide to Morse code symbols, see morse_tree.png
+Go left for dots and right for dashes
--- generate_morse_dataset.py
+++ generate_morse_dataset.py
@@ -1,25 +1,30 @@
+#==============================================================================
 # Created by Sourya Dey, USC
+#==============================================================================
 
 import numpy as np
-def generate_morse_dataset(Style='GRAY', Framelen = 64, Classes = 64,
-                           TReach = 5000, VAeach = 1000, TEeach = 1000,
-                           minlendot = 1, maxlendot = 3, minlendash = 4, maxlendash = 9, minlensp = 1, maxlensp = 3, leadingsp_rand=0,
-                           maxflip = 0,
-                           levels = 16, symbmean = 12, symbsd = 1.34, noisemean = 0, noisesd = 1):
+def store_morse_data(Style='GRAY', save_filename = '',
+                     Framelen = 64, dilation=1, Classes = 64,
+                     TReach = 5000, VAeach = 1000, TEeach = 1000,
+                     minlendot = 1, maxlendot = 3, minlendash = 4, maxlendash = 9, minlensp = 1, maxlensp = 3, leadingsp_rand=0,
+                     maxflip = 0,
+                     levels = 16, symbmean = 12, symbsd = 1.34, noisemean = 0, noisesd = 0):
     '''
     Create morse code datasets:
         Style='BW': Black and white having 0s for spaces and 1s for dot or dash. Noise means bit flips
         Style='GRAY': Gaussian grayscale levels. Noise is additive Gaussian
+        save_filename: Leave as '' to save in preset path inside
 
-    Framelen: Total length of frame for 1 character, i.e. 1 input
-    Classes: How many characters, i.e. outputs
+    Framelen: Total length of frame for 1 character
+    Classes: How many characters
     TReach,VAeach,TEeach: No. of training, validation and test cases FOR EACH class
     minlendot,maxlendot,... : min and max length of dots, dashes and intermediate spaces
     leadingsp_rand: Set to 0 to have no leading spaces, otherwise 1 to have random number of leading spaces
+    dilation: If >1, all lengths are increased by this factor
 
     Black and white only:
         maxflip: Noise measure. Max how many bits to flip
-        SET maxfilp=0 for NO NOISE
+        SET maxflip=0 for NO NOISE
     Grayscale only:
         levels: How many levels for dots and dashes. Will be normalized at end
         symbmean: Mean level for any symbol (dot or dash)
@@ -37,12 +42,19 @@ def generate_morse_dataset(Style='GRAY', Framelen = 64, Classes = 64,
     TEcases = TEeach*Classes
     TOTcases = TOTeach*Classes
 
+    Framelen *= dilation
+    minlendot *= dilation
+    maxlendot *= dilation
+    minlendash *= dilation
+    maxlendash *= dilation
+    minlensp *= dilation
+    maxlensp *= dilation
+
     xdata = np.zeros((TOTcases,Framelen),dtype='float')
     ydata = np.zeros((TOTcases,Classes),dtype='float')
     index = 0
 
     for n in xrange(len(Codebook)):
-        print 'Starting {0}/{1} class'.format(n+1,len(Codebook)) #progress...
         code = Codebook.values()[n]
 
         for repeat in xrange(TOTeach):
@@ -89,13 +101,16 @@ def generate_morse_dataset(Style='GRAY', Framelen = 64, Classes = 64,
 
             index += 1
 
+        print '{0}/{1}'.format(n+1,Classes) #progress...
+
     ### Post-processing
     if Style=='GRAY':
         levels = float(levels)
-        xdata = np.around(xdata)
+        #xdata = np.around(xdata) #Round before so that final xdata has values = n/16, n in [0,16]
         xdata[xdata>levels] = levels
-        xdata[xdata<=0] = 0.
+        xdata[xdata<0] = 0.
         xdata /= levels #set range to 0-1
+        xdata = np.around(xdata, 3) #Round after
 
     ### Split into training, validation, test
     xtr = np.zeros((TRcases,Framelen))
@@ -124,8 +139,24 @@ def generate_morse_dataset(Style='GRAY', Framelen = 64, Classes = 64,
     yte = yte[n[:]]
 
     ### Save
-    np.savez_compressed('./morse_dataset.npz', xtrain=xtr,ytrain=ytr, xval=xva,yval=yva, xtest=xte,ytest=yte)
+    if save_filename!='':
+        np.savez_compressed(save_filename, xtr=xtr,ytr=ytr,xva=xva,yva=yva,xte=xte,yte=yte)
+    else:
+        if Style=='BW':
+            np.savez_compressed('./morseBW{0}_{1}i{2}o_noise{3}_lsp{4}.npz'.format(TOTcases,Framelen,Classes,maxflip,leadingsp_rand), xtr=xtr,ytr=ytr,xva=xva,yva=yva,xte=xte,yte=yte)
+        elif Style=='GRAY':
+            np.savez_compressed('./morseGRAY{0}_{1}i{2}o_noisesd{3}_lsp{4}_dash{5}{6}.npz'.format(TOTcases,Framelen,Classes,noisesd,leadingsp_rand,minlendash,maxlendash), xtr=xtr,ytr=ytr,xva=xva,yva=yva,xte=xte,yte=yte)
 
 
-###### EXECUTION ######
-generate_morse_dataset()
+#==============================================================================
+# Get baseline dataset
+#==============================================================================
+store_morse_data(save_filename='./baseline.npz')
+
+#==============================================================================
+# Get a really difficult dataset with
+# noise, leading spaces, and dashes confused as dots and spaces
+#==============================================================================
+store_morse_data(save_filename='./difficult.npz', minlendash=3, leadingsp_rand=1, noisesd=4)
+
+
